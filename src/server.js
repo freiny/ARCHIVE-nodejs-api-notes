ea0b4c79bae61bfc7418ecfbfff9d89a33171e9f
name <freiny50@gmail.com>
2016-08-09 08:00:39 -0500

    Update server.js: add initial vm code

--- src/server.js
+++ src/server.js
@@ -4,10 +4,29 @@ const express = require('express');
 const PORT = process.env.HTTP_PORT;
 
 const app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!\n');
-	console.log("ping");
-});
+app.get('/', onRoot);
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
+
+
+function onRoot(req, res){
+	var a = "hello global server world";
+	const vm = require('vm');
+	const script = new vm.Script(
+		'console.log("Hello VM World");'
+		, {
+	  filename: 'main.js', // filename for stack traces
+	  lineOffset: 1,
+	  columnOffset: 1,
+	  displayErrors: true,
+	  timeout: 1000 // ms
+	});
+	var out = script.runInThisContext()
+
+	res.send('Hello World!\n');
+	console.log("ping");
+
+
+
+}
