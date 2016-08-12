c94cdfff08d0cdda3600059905bf7674e844991a
name <freiny50@gmail.com>
2016-08-12 15:54:04 -0500

    Update server.js: allow frontend to find backend services on bridged network

--- src/server.js
+++ src/server.js
@@ -4,15 +4,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', onRoot);
+app.get('/api', onAPI);
 app.get('/api/test', apiTest);
 app.listen(PORT);
 appReady()
-
-function appReady(){
-	if (process.env.APP_ENVIRONMENT === 'dev'){
-		console.log('****************** App Ready');
-	}
-}
 
 function onRoot(req, res){
 	var a = 'hello global server world';
@@ -35,4 +30,25 @@ function onRoot(req, res){
 function apiTest(req, res) {
 	console.log('BE ping apiTest()');
 	res.json({'msg':'BE apiTest() json response'});
+}
+
+function onAPI(req, res) {
+	console.log('BE ping apiTest()');
+	res.json({'name':'beapi', 'port':PORT});
+}
+
+function appReady(){
+	if (process.env.APP_ENVIRONMENT === 'dev'){
+		console.log('****************** App Ready');
+	}
+}
+
+function getIP(){
+	var ifaces = require('os').networkInterfaces();
+	var address = Object.keys(ifaces).reduce(function (result, dev) {
+		return result.concat(ifaces[dev].reduce(function (result, details) {
+			return result.concat(details.family === 'IPv4' && !details.internal ? [details.address] : []);
+		}, []));
+	});
+	return address.toString().substring(2);
 }
